187efa43a879d25ebadacbcfb6702ac478447d85
Usama Muneer <usama@rentable.pk>
2022-09-15 01:56:24 +0100

    data extraction.

--- app/Actions/GetTeamsFromTitle.php
+++ app/Actions/GetTeamsFromTitle.php
@@ -6,9 +6,29 @@ class GetTeamsFromTitle
 {
     public function __invoke(string $title): array
     {
-        // TODO: Extract the two teams that played in the game from the title of the video and return them as an array.
-        // example: return [1, 3];
-        // example: return ['A', 'F'];
-        return ['TODO', 'TODO'];
+        //check if brackets return null
+        if (!preg_match('!\(([^\)]+)\)!', $title, $match)) {
+            return [null, null];
+        }
+        $string = explode(')', (explode('(', $title)[1]))[0];
+
+        //check if string has letter T.
+        if (str_contains($string, 'T')) {
+            $teams = explode(' v ', $string);
+            $data = [];
+            foreach ($teams as $team) {
+                $st = trim($team, "T");
+                $data[] = $st;
+            }
+            return array_values($data);
+        }
+        //for the rest of the TEAMS.
+        $teams = explode(' v ', $string);
+        $data = [];
+        foreach ($teams as $team) {
+            $st = substr($team, 0, 1);
+            $data[] = $st;
+        }
+        return array_values($data);
     }
 }
